a049cf0703854725cb1ec5ff48dd38bb38631a8a
sp42 <sp42@qq.com>
2024-11-15 14:52:55 +0800

    ci

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# aj-spring
-Spring Enhancement Framework
+# Spring Enhancement Framework
+For fast using Spring Boot, some common components should be adding first.
+
+By using `spring-boot-starter-web`, here are the components by default:
+
+- Spring Boot
+- Logback and Slf4j
+- Jackson JSON parser
+
+We add some components:
+
+- 获得 Controller 方法名、请求参数和注解信息
+- Global Response Object
+- Global Exception Handler
+- Spring IoC Tools
---
+++ src/main/java/com/ajaxjs/springboot/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.ajaxjs.springboot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class GlobalExceptionHandler implements HandlerExceptionResolver {
+    @Override
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        return null;
+    }
+}
